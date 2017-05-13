53f27c33ac2148c85bb8249a9b19f528d45c5450
Thomas Charbonnel <thomas@charbonnel.eu>
2017-05-13 22:37:27 +0800

    Tried fix server_test

--- server_test.go
+++ server_test.go
@@ -16,15 +16,59 @@ func ExampleGenerateHTML() {
   //   <meta name="author" content="Thomas Charbonnel">
   // 
   //   <style>
+  //     body {
+  //       background-color: black;
+  //     }
+  // 
+  // 
   //     #frame {
-  //       height: 50%;
-  //       width: 50%;
-  //       margin: 0 auto;
+  //       width: 100vw;
+  //       height: 56.25vw;
+  //       max-height: 100vh;
+  //       max-width: 177.78vh;
+  //       margin: auto;
+  //       position: absolute;
+  //       top:0;bottom:0;
+  //       left:0;right:0;
+  //     }
+  // 
+  //     img#background {
+  //       position: absolute;
+  //       width: 100%;
+  //       height: 100%;
+  //     }
+  // 
+  //     #headline {
+  //       position: absolute;
+  //       background-color: white;
+  //       z-index: 100;
+  //       bottom: 2%;
+  //       width: 80%;
+  //       padding: 1%;
+  //       margin: 1%;
+  //       font-size: 24pt;
+  //       font-weight: bold;
+  //     }
+  // 
+  //     img#logo {
+  //       position: absolute;
+  //       z-index: 100;
+  //       background-color: white;
+  //       bottom: 2%;
+  //       right: 0%;
+  //       width: 13%;
+  //       padding: 1%;
+  //       margin: 1%;
+  //       height: 7%;
   //     }
   //   </style>
   // </head>
   // <body>
-  //   <div id="frame">lol</div>
+  //   <div id="frame">
+  //     <img id="background" src="pony">
+  //     <p id="headline">lol</p>
+  //     <img id="logo" src="./trump_logo.jpg">
+  //   </div>
   // </body>
   // </html>
 }
